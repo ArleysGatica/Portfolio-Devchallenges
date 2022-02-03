b8a13b34170921677cca573ce8df2a770dfb537a
ArleysGatica <gatica.arleys16@gmail.com>
2022-02-03 00:02:25 -0600

    adding percentage bars

--- src/app/components/Front-End/front-end.jsx
+++ src/app/components/Front-End/front-end.jsx
@@ -14,8 +14,30 @@ export const FrontEnd = props => {
           {tecnologia &&
             tecnologia.map((item, index) => <p key={index}>{item}</p>)}
         </div>
-        <div className="rayas"></div>
-        <div className="rayas2"></div>
+        <div className="Html">
+          <div className="rayas"></div>
+          <div className="rayas2"></div>
+        </div>
+        <div className="css">
+          <div className="rayas-css"></div>
+          <div className="rayas2-css"></div>
+        </div>
+        <div className="js">
+          <div className="rayas-js"></div>
+          <div className="rayas2-js"></div>
+        </div>
+        <div className="react">
+          <div className="rayas-react"></div>
+          <div className="rayas2-react"></div>
+        </div>
+        <div className="node">
+          <div className="rayas-node"></div>
+          <div className="rayas2-node"></div>
+        </div>
+        <div className="git">
+          <div className="rayas-git"></div>
+          <div className="rayas2-git"></div>
+        </div>
       </div>
     </>
   );
